d82ac759d0f45104dd33a2b60b9366269fc34302
faqihfnf <faqih.fnf@gmail.com>
2024-09-02 00:38:47 +0700

    Pages & Router

---
+++ src/components/layout/AppShell/index.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../Navbar";
+
+type AppShellProps = {
+  children: React.ReactNode;
+};
+
+const AppShell = (props: AppShellProps) => {
+  const { children } = props;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default AppShell;
